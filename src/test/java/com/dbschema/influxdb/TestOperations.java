bf0d2aa07a11891907d418ba840de7ba6c50975b
wise-coders <support@dbschema.com>
2022-01-07 10:20:24 +0100

    First commit

--- src/test/java/com/dbschema/influxdb/TestOperations.java
+++ src/test/java/com/dbschema/influxdb/TestOperations.java
@@ -1,7 +1,5 @@
 package com.dbschema.influxdb;
 
-import com.influxdb.annotations.Column;
-import com.influxdb.annotations.Measurement;
 import com.influxdb.client.*;
 import com.influxdb.client.domain.Bucket;
 import com.influxdb.client.domain.WritePrecision;
@@ -11,70 +9,93 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class TestOperations {
 
-    //private static char[] token = "Qzbb07b2GVs83F1jDs6ayP3hi37qyj8hufY6tWeUz9pt9nyGbXA-dpp68Rlbl7WxQLR9qMFpkqB1TGpBie49iA==".toCharArray();
-
-    //This is Ivan's Token
-    private static char[] token = "5CbH7vMJ-tx6p68E4egEcKDLMCE0Nd64AYpS2J5zzDc68ddOQD0Thq4gXPUIUut__rVuo37-6MHRUZ3_4GwkHw==".toCharArray();
-    private static String org = "dbschema";
-    private static String bucket = "sample";
-
     private InfluxDBClient influxDBClient;
-
+    private final Properties prop = new Properties();
 
     @After
-    public void closeConnection() throws SQLException {
+    public void closeConnection() {
         influxDBClient.close();
     }
     @Before
-    public void testDriver() throws SQLException {
+    public void testDriver() throws IOException {
 
-        this.influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
+        prop.load( new FileInputStream("gradle.properties"));
+
+        this.influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", prop.getProperty("token").toCharArray(), prop.getProperty("org"), prop.getProperty("bucket"));
 
         WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
 
-        writeApi.writeRecord(WritePrecision.NS, "persons,firstname=Mihai value=10.0");
+        writeApi.writeRecord(WritePrecision.NS, "persons,firstname=John,lastname=Parker value=10.0");
         writeApi.writeRecord(WritePrecision.NS, "persons,firstname=Dan value=10.0");
 
-        /*
-        // Write by Data Point
-        Point point = Point.measurement("temperature")
-                .addTag("location", "west")
-                .addField("value", 55D)
-                .time(Instant.now().toEpochMilli(), WritePrecision.MS);
+    }
 
-        writeApi.writePoint(point);
 
-        // Write by LineProtocol
-        writeApi.writeRecord(WritePrecision.NS, "temperature,location=north value=60.0");
+    /*
+    This Test function gets the buckets for the given organisation
+    Buckets are Analogous to schemas within an traditional RDBMS
+    Org is similar in to a database in that its a group of Buckets
+     */
+    @Test
+    public void listBuckets() throws SQLException {
+        BucketsApi bucketApi = influxDBClient.getBucketsApi();
+        List<Bucket> buckets = bucketApi.findBucketsByOrgName(prop.getProperty("org"));
+        for (Bucket fluxTable : buckets) {
+            System.out.printf("Bucket " + fluxTable.toString());
+        }
+        influxDBClient.close();
+    }
 
-        // Write by POJO
-        Temperature temperature = new Temperature();
-        temperature.location = "south";
-        temperature.value = 62D;
-        temperature.time = Instant.now();
+    /*
+    List measurements (similar with tables) and its columns
+     */
+    @Test
+    public void listMeasurementAndFieldNames() throws SQLException {
 
-        writeApi.writeMeasurement( WritePrecision.NS, temperature);
-        */
+        String bucket = prop.getProperty("bucket");
+        QueryApi queryApi = influxDBClient.getQueryApi();
 
+        String flux = "import \"influxdata/influxdb/schema\"\n" +
+                "\n" +
+                "schema.measurements(bucket: \"" + bucket + "\")";
+
+        for (FluxTable fluxTable : queryApi.query(flux)) {
+            for (FluxRecord fluxRecord : fluxTable.getRecords() ) {
+
+                String measurement = String.valueOf( fluxRecord.getValueByKey("_value") );
+                System.out.println("#### Measurement " + measurement);
+                String flux2 = "import \"influxdata/influxdb/schema\"\n" +
+                        "\n" +
+                        "schema.measurementFieldKeys(bucket: \"" + bucket + "\", measurement: \"" + measurement + "\")";
+
+                for (FluxTable fluxTable2 : queryApi.query(flux2)) {
+                    for (FluxRecord fluxRecord2 : fluxTable2.getRecords()) {
+                        String column = String.valueOf( fluxRecord2.getValueByKey("_value") );
+                        System.out.println("  Column " + column );
+                    }
+                }
+            }
+        }
     }
 
 
     @Test
-    public void test() throws SQLException {
+    public void testQuery() {
         String flux = "from(bucket:\"sample\") |> range(start: 0)";
 
         QueryApi queryApi = influxDBClient.getQueryApi();
 
         List<FluxTable> tables = queryApi.query(flux);
         for (FluxTable fluxTable : tables) {
-            System.out.printf("Table " + fluxTable.toString());
+            System.out.println("### Table " + fluxTable.toString());
             List<FluxRecord> records = fluxTable.getRecords();
             for (FluxRecord fluxRecord : records) {
                 System.out.println(fluxRecord.getTime() + ": " + fluxRecord.getValues());
@@ -84,36 +105,8 @@ public class TestOperations {
         influxDBClient.close();
     }
 
+/*
     @Test
-    /*
-    This Test function gets the buckets for the given organisation
-    Buckets are Analogous to schemas within an traditional RDBMS
-    The org is similar in to a database in that its a group of Buckets
-
-     */
-    public void getBuckets() throws SQLException {
-
-
-        BucketsApi bucketApi = influxDBClient.getBucketsApi();
-
-
-
-
-        List<Bucket> buckets = bucketApi.findBucketsByOrgName(org);
-        for (Bucket fluxTable : buckets) {
-            System.out.printf("Bucket " + fluxTable.toString());
-        }
-
-        influxDBClient.close();
-
-
-
-    }
-    @Test
-    /*
-    This Test function gets the measurements and its columns for the sample bucket
-
-     */
     public void getMeasurementAndFieldNames() throws SQLException {
 
         List<String> Measurements = getMeasurements("sample");
@@ -148,8 +141,6 @@ public class TestOperations {
                 values.add(fluxRecord.getValueByKey("_value").toString());
             }
         }
-
-     //   influxDBClient.close();
         return values;
     }
 
@@ -172,26 +163,8 @@ public class TestOperations {
                 values.add(fluxRecord.getValueByKey("_value").toString());
             }
         }
-
-      //  influxDBClient.close();
         return values;
     }
-
-
-
-    @Measurement(name = "temperature")
-    private static class Temperature {
-
-        @Column(tag = true)
-        String location;
-
-        @Column
-        Double value;
-
-        @Column(timestamp = true)
-        Instant time;
-    }
-
-
+*/
 
 }
